7b19cabb09f481a2c243ba8f46a798c04548c17f
harshulvijay <64775657+harshulvijay@users.noreply.github.com>
2021-10-11 15:39:05 +0530

    feat: add function for initializing the app

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,12 @@ import "./style.scss";
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
-app.innerHTML = ``;
+/**
+ * Initializes the app
+ * 
+ * @param {T extends HTMLElement = HTMLDivElement} root app's root component
+ */
+const init = <T extends HTMLElement = HTMLDivElement>(root: T) => {
+};
+
+init<HTMLDivElement>(app);

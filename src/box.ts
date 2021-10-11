415535e6ee6d1dc423d09d2b898a00f6b4bbbd61
harshulvijay <64775657+harshulvijay@users.noreply.github.com>
2021-10-11 15:32:20 +0530

    feat: add function for creating a box

---
+++ src/box.ts
@@ -0,0 +1,14 @@
+/**
+ * Creates a box with text `t`
+ *
+ * @param {number} t text to display inside the box and use for its id
+ * @returns {HTMLDivElement} box element
+ */
+const createBox = (t: string): HTMLDivElement => {
+  const box = document.createElement("div");
+  box.classList.add("box");
+  box.id = `box box--${t}`;
+  box.innerText = t;
+
+  return box;
+};

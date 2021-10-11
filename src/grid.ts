b0b87cad9aa81381d0deec6cdee8d33e82629c77
harshulvijay <64775657+harshulvijay@users.noreply.github.com>
2021-10-11 15:32:49 +0530

    feat: add function for creating a grid with boxes

---
+++ src/grid.ts
@@ -0,0 +1,17 @@
+/**
+ * Creates a grid element with `n` boxes and appends it to `p`
+ *
+ * @param {HTMLElement} p parent element of the grid
+ * @param {number} n number of boxes in the grid
+ */
+const createBoxGrid = (p: HTMLElement, n: number) => {
+  const grid = document.createElement("div");
+  grid.classList.add("grid");
+
+  for (let i = 0; i < n; i++) {
+    const box = createBox(i.toString());
+    grid.append(box);
+  }
+
+  p.append(grid);
+};

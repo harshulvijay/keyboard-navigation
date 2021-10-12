be17f14469c8c94739097144b18a21f1c390836c
harshulvijay <64775657+harshulvijay@users.noreply.github.com>
2021-10-12 19:59:26 +0530

    feat: add support for key combinations
    key combinations of the form `<modifier key> + <alphanumeric key>` are now supported

--- src/navigation.ts
+++ src/navigation.ts
@@ -30,11 +30,29 @@ interface KbNavBehaviorParams<T = HTMLElement> {
 type KbNavBehavior<T = HTMLElement> = (o: KbNavBehaviorParams<T>) => void;
 
 /**
+ * Enum of [modifier keys](https://en.wikipedia.org/wiki/Modifier_key)
+ */
+enum ModifierKeys {
+  ALT = "alt",
+  CTRL = "control",
+  META = "meta",
+  SHIFT = "shift",
+}
+
+/**
  * Navigation behavior map
  *
  * @type {Record<string, KbNavBehavior>}
  */
 const navBehaviorMap: Record<string, KbNavBehavior> = {};
+
+/**
+ * Array of modifier keys
+ *
+ * Generated from the `ModifierKeys` enum
+ * (see https://stackoverflow.com/a/57266281)
+ */
+const modKeys: string[] = Object.values(ModifierKeys);
 
 /**
  * Returns the width of `el`
@@ -66,6 +84,82 @@ export const focusOn = (nc: HTMLElement, f: string, i: number) => {
 };
 
 /**
+ * Checks if `k` is a modifier key
+ *
+ * @param {string} k name of the key
+ * @returns {boolean}
+ */
+export const isModifierKey = (k: string): boolean => {
+  return modKeys.findIndex((mk) => k.toLowerCase() === mk) !== -1;
+};
+
+/**
+ * Class for using key combinations (i.e, of the form `<modifier key> +
+ * <alphanumeric key>`)
+ */
+export class KeyCombo {
+  /**
+   * String of keys separated by a delimiter `d`
+   *
+   * Generated from `KeyCombo.keys`
+   */
+  combo: string;
+
+  /**
+   * Keys involved in the key combo
+   */
+  keys: string[];
+
+  /**
+   * Constructor
+   *
+   * @param {string[]} k keys involved in the key combo
+   */
+  constructor(...k: string[]) {
+    // convert each key name to lowercase
+    this.keys = k.map((v) => v.toLowerCase());
+    this.combo = KeyCombo.generateKeyCombo(`+`, ...this.keys);
+  }
+
+  /**
+   * Generates combo string of `k` separated by `d`
+   *
+   * @param {string} d delimiter
+   * @param {string[]} k array of key names
+   * @returns {string} key combo
+   */
+  static generateKeyCombo(d: string, ...k: string[]): string {
+    return k
+      .filter((key) => key)
+      .join(d)
+      .toLowerCase();
+  }
+
+  /**
+   * Generates key combo from `e`
+   *
+   * @param {KeyboardEvent} e keyboard event
+   * @param {string} d delimiter used in the key combo
+   * @returns {string | null} key combo or `null`, in case the key pressed is a
+   * modifier key
+   */
+  static getKeyCombo(e: KeyboardEvent, d: string = `+`): string | null {
+    if (isModifierKey(e.key)) return null;
+
+    // alt
+    const a = e.altKey ? ModifierKeys.ALT : "";
+    // control
+    const c = e.ctrlKey ? ModifierKeys.CTRL : "";
+    // meta
+    const m = e.metaKey ? ModifierKeys.META : "";
+    // shift
+    const s = e.shiftKey ? ModifierKeys.SHIFT : "";
+
+    return this.generateKeyCombo(d, a, c, m, s, e.key.toLowerCase());
+  }
+}
+
+/**
  * Checks if the key pressed is a modifier key
  *
  * @param {KeyboardEvent} e keyboard event
@@ -79,15 +173,25 @@ export const modifierKeyPressed = (e: KeyboardEvent): boolean => {
  * Adds behavior `f` for key(s) `k`, and "stacks" behaviors if `s` is set to
  * `true`
  *
- * @todo add support for key combinations
- *
- * @param {string[]} k key(s) to bind the behavior to
+ * @param {(string | KeyCombo)[]} k key(s) to bind the behavior to
  * @param {KbNavBehavior} f behavior
  * @param {boolean} s whether to "stack" behaviors on top of each other
  */
-export const addKbNavBehavior = (k: string[], f: KbNavBehavior, s: boolean) => {
+export const addKbNavBehavior = (
+  k: (string | KeyCombo)[],
+  f: KbNavBehavior,
+  s: boolean
+) => {
   k.map((item) => {
-    const key = item.toLowerCase();
+    let key: string = "";
+
+    if (item instanceof KeyCombo) {
+      // it is a key combo (`KeyCombo`)
+      ({ combo: key } = item);
+    } else {
+      // it is a key name as a plain string
+      key = item.toLowerCase();
+    }
 
     if (navBehaviorMap[key]) {
       // behavior for `key` already exists
@@ -103,7 +207,9 @@ export const addKbNavBehavior = (k: string[], f: KbNavBehavior, s: boolean) => {
           f(o);
         };
       } else {
-        throw new Error(`behavior for key "${key}" has already been defined`);
+        throw new Error(
+          `behavior for key/combo "${key}" has already been defined`
+        );
       }
     } else {
       navBehaviorMap[key] = f;
@@ -142,12 +248,13 @@ export const addKbNav = (nc: HTMLElement, s: string) => {
     };
 
     nc.onkeydown = (e) => {
-      // skip modifier keys
-      if (!modifierKeyPressed(e)) {
-        // stop the default behavior of the key
-        e.preventDefault();
+      const key = KeyCombo.getKeyCombo(e);
 
-        const { key, target } = e;
+      if (key) {
+        // it is a valid key (combo)!
+        // prepare for execution of the defined behavior
+
+        const { target } = e;
         const targetEl = target as HTMLElement;
 
         const behavior = navBehaviorMap[key.toLowerCase()];
@@ -163,7 +270,11 @@ export const addKbNav = (nc: HTMLElement, s: string) => {
         };
 
         // execute the behavior
-        behavior?.bind(undefined, params).call(undefined);
+        if (behavior) {
+          behavior.bind(undefined, params).call(undefined);
+          // stop the default behavior of the key
+          e.preventDefault();
+        }
       }
     };
   }

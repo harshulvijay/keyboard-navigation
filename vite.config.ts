671e619a10c9c365914a297ae2fbde96617b3e6e
harshulvijay <64775657+harshulvijay@users.noreply.github.com>
2021-11-11 00:33:53 +0530

    refactor: rename `src` to avoid confusion (#32)
    closes #31

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: ".",
+  root: "./testing/manual",
   esbuild: {},
   build: {
     outDir: "./build",

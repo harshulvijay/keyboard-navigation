ed9908c1641751eb02d9699acc3733070e2f2c5a
harshulvijay <64775657+harshulvijay@users.noreply.github.com>
2021-11-11 22:01:17 +0530

    tests: add manual tests for vanilla javascript via `vite` (#36)

---
+++ testing/manual/vite.config.ts
@@ -0,0 +1,16 @@
+// common configuration options
+
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: "./build",
+  },
+  resolve: {
+    alias: {
+      "@lib": resolve(__dirname, "../../lib"),
+    },
+  },
+});

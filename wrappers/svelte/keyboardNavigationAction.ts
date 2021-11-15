ed976ccfb21998e2e587dff4031e95567504f1a6
harshulvijay <64775657+harshulvijay@users.noreply.github.com>
2021-11-15 13:48:19 +0530

    docs: add documentation in code for the svelte action

--- wrappers/svelte/keyboardNavigationAction.ts
+++ wrappers/svelte/keyboardNavigationAction.ts
@@ -1,13 +1,25 @@
 import { KeyMap } from "../../lib/keyMap";
 import { addKbNav } from "../../lib/navigation";
 
-export function keyboardNavigation(
-  node: HTMLElement,
-  options: {
-    keymap: KeyMap;
-    selector: string;
-  }
-) {
+export interface ActionOptions {
+  /**
+   * @param {KeyMap<T extends HTMLElement>} keymap key map to use
+   */
+  keymap: KeyMap;
+
+  /**
+   * @param {string} selector selector that can select any direct child of `nc`
+   */
+  selector: string;
+}
+
+/**
+ * Keyboard navigation Svelte action
+ *
+ * @param {HTMLElement} node container to add keyboard navigation to
+ * @param {ActionOptions} options options
+ */
+export function keyboardNavigation(node: HTMLElement, options: ActionOptions) {
   addKbNav(options.keymap, node, options.selector);
 }
 

dcf01c125243852d21bfd0b42a5dd107628c20a5
harshulvijay <64775657+harshulvijay@users.noreply.github.com>
2021-11-16 20:13:33 +0530

    docs: add comments

--- demo/svelte/src/App.svelte
+++ demo/svelte/src/App.svelte
@@ -2,12 +2,22 @@
   import { focusOn } from "@utils/dom";
   import { keyboardNavigation, KeyMap } from "mylib/wrappers/svelte";
 
+  // the element on which the action will be listening for key presses
   let navigationContainer: HTMLElement;
+  // create an array of numbers from 0 to 99
   const array = [...Array(100).keys()];
+
+  // create a keymap
   const keymap = new KeyMap();
 
+  // attach a new behavior
   keymap.attachBehavior(["arrowup", "w"], {
+    // this function will execute when either "w" or the "arrowup" key is
+    // pressed
     behavior: (o) => {
+      // `o` contains some properties that may be useful, for example, 
+      // `position` if the element that's being focused on has a `data-number`
+      // property
       const next = o.target.position - o.elements;
       focusOn(navigationContainer, "number", next);
     },
@@ -37,10 +47,17 @@
 </script>
 
 <main>
+  <!-- bind this `div` to the `navigationContainer` variable -->
+  <!-- the `keyboardNavigation` action will listen for key presses on this 
+    element -->
   <div
     bind:this={navigationContainer}
     use:keyboardNavigation={{
+      // pass the keymap that we defined earlier
       keymap,
+      // will work for all elements that have a class `box`
+      // note: currently, all `.box`es need to have the same width, as it is,
+      // as of now, a limitation of the svelte action
       selector: ".box",
     }}
     class="grid"
@@ -52,9 +69,11 @@
 </main>
 
 <style lang="scss">
+  // styling
+
   $box-color: #f44336;
   $box-focused-outline-color: #2196f3;
-  
+
   :root {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
       Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
